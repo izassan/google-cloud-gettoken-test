b5de92a54c658d3771f5bbd4aad694998a10566a
izassan <izassan.si@gmail.com>
2024-10-15 00:16:42 +0900

    init: added test program

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Google.Apis.Auth.OAuth2;
+
+class MainClass
+{
+    static void Main()
+    {
+        try{
+            GoogleCredential credential = GoogleCredential.GetApplicationDefault();
+            ICredential icred = credential.UnderlyingCredential;
+            string token = icred.GetAccessTokenForRequestAsync().Result;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("start getting credential");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"token: {token}");
+        }catch(Exception e){
+            Console.WriteLine($"{e.Message}");
+            Console.WriteLine($"{e.StackTrace}");
+        }finally{
+            Console.ResetColor();
+            Console.WriteLine("finish");
+        }
+    }
+}
